cb1ff1162615f75073af5c702dc626a7d9c7be25
codexsolutions-tech <codexsolutions9@gmail.com>
2026-07-29 19:58:09 -0300

    feat: ajuste clique notificacao

--- public/sw.js
+++ public/sw.js
@@ -6,6 +6,29 @@ self.addEventListener("activate", () => {
     console.log("Service Worker ativo");
 });
 
+self.addEventListener("notificationclick", (event) => {
+    event.notification.close();
+
+    const url = event.notification.data?.url || "/";
+
+    event.waitUntil(
+        clients.matchAll({
+            type: "window",
+            includeUncontrolled: true
+        }).then((clientList) => {
+
+            for (const client of clientList) {
+                if ("focus" in client) {
+                    client.navigate(url);
+                    return client.focus();
+                }
+            }
+
+            return clients.openWindow(url);
+        })
+    );
+});
+
 
 self.addEventListener("push", (event) => {
 
