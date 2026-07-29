aaa02226fa6afe56b4f5f4c54b3df33f35600fdf
codexsolutions-tech <codexsolutions9@gmail.com>
2026-07-29 14:50:49 -0300

    fix notification

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { registerSW } from "virtual:pwa-register";
+//import { registerSW } from "virtual:pwa-register";
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
 import { AuthProvider } from "./context/AuthContext";
@@ -9,7 +9,7 @@ import { ToastProvider } from "./context/ToastContext";
 import "./index.css";
 
 // Registra o service worker (atualiza automaticamente quando há nova versão)
-registerSW({ immediate: true });
+//registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
